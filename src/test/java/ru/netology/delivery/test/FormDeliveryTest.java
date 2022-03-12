ac73c50843898c24dd0d8fe695dbcc904020b950
iiigogor <92747953+iiigogor@users.noreply.github.com>
2022-03-12 21:26:10 +0300

    Update FormDeliveryTest.java
    commit 3

--- src/test/java/ru/netology/delivery/test/FormDeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/FormDeliveryTest.java
@@ -47,8 +47,7 @@ public class FormDeliveryTest {
         var firstMeetingDate = generateDate(daysToAddForFirstMeeting);
         var daysToAddForSecondMeeting = 10;
         var secondMeetingDate = generateDate(daysToAddForSecondMeeting);
-       // Faker faker = new Faker(new Locale("ru"));
-
+       
         $("[data-test-id=city] input").setValue(DataGenerator.generateCity());
         $("[data-test-id=date] input").sendKeys(DataGenerator
                 .generateDate(daysToAddForFirstMeeting));
